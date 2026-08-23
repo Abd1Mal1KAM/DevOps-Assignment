ddbb77d66e4dbee5a482e3a310b4fc897736ef50
Abdimalik Abdi Mohamed <Abdimalik.AbdiMohamed@apollounderwriting.com>
2026-08-23 22:08:17 +0100

    modified terraform code

--- terraform/data.tf
+++ terraform/data.tf
@@ -2,8 +2,20 @@ data "aws_ami" "ubuntu" {
   most_recent = true
   owners      = [var.ubuntu_account_id]
 
-  # filter {
-  #   name   = "name"
-  #   values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-20240801,ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-20240715"]
-  # }
+  filter {
+    name   = "architecture "
+    values = ["i386", "x86_64"]
+  }
+}
+
+data "aws_iam_policy_document" "instance_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    effect  = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
 }
--- terraform/user.tf
+++ terraform/user.tf
@@ -1,16 +1,3 @@
-data "aws_iam_policy_document" "instance_assume_role_policy" {
-  statement {
-    actions = ["sts:AssumeRole"]
-    effect  = "Allow"
-
-    principals {
-      type        = "Service"
-      identifiers = ["ec2.amazonaws.com"]
-    }
-  }
-}
-
-
 resource "aws_iam_role" "ec2_role" {
   assume_role_policy = data.aws_iam_policy_document.instance_assume_role_policy.json
 }
